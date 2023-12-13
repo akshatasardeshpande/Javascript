95670eac7253dcea5da2c038878b8aaa17cea422
Akshata <akshatasag@gmail.com>
2023-12-13 11:14:35 +0530

    Update Ten.js

--- JS_Practice/Ten.js
+++ JS_Practice/Ten.js
@@ -1,3 +1,43 @@
 // 10. Boolean value is either true or false.
-//     - Write three JavaScript statement which provide truthy value.
-//     - Write three JavaScript statement which provide falsy value.
+//     - Write three JavaScript statements which provide truthy value.
+//     - Write three JavaScript statements which provide falsy value.
+
+// let guess = 4;
+// if (guess) 
+// { 
+//     console.log("Truthy 1");
+// } // truthy
+
+// guess = 'four';
+// if (guess) 
+// {
+//     console.log("Truthy 2");
+// } // truthy
+  
+let Guess = false;
+if (Guess) 
+{ 
+    console.log("Falsy 1");
+} // falsy
+
+// let Guess2 = 0;
+// if (Guess2) 
+// {
+//     console.log("Falsy 2");
+// } // A value of 0 (zero) is also falsy.
+
+// let Guess3 = "";
+// if (Guess3){
+// console.log("Falsy 3");
+// } //A "", which is an empty string, is falsy.
+
+// let Guess4 = null;
+// if(Guess4){
+//     console.log("Falsy 4");
+// } //A null variable is also falsy.
+
+// let Guess5 = Number("four"); // NaN
+// if(Guess5){
+//     console.log("Falsy 5");
+// } // Variables that are NaN evaluate to falsy.
+
