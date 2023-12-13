fb91c6a25f040dad91abfc24737f052e600d7119
Akshata <akshatasag@gmail.com>
2023-12-13 10:05:25 +0530

    Created Two.js

---
+++ JS_Practice/Two.js
@@ -0,0 +1,14 @@
+// 2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstname ="Akshata";
+let lastname = "Sardeshpande";
+let marital_status = "UM";
+let country = "India";
+let age = 26;
+
+console.log(`FIRST NAME : ${firstname}
+LAST NAME: ${lastname}
+MARITAL_STATUS: ${marital_status}
+COUNTRY: ${country}
+AGE: ${age}
+`);
