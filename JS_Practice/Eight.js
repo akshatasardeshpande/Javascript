b8a786f9c9aa3efefb4fe3331018c5b95534b3de
Akshata <akshatasag@gmail.com>
2023-12-13 10:48:25 +0530

    Update Eight.js

--- JS_Practice/Eight.js
+++ JS_Practice/Eight.js
@@ -1,1 +1,10 @@
-// 8. Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'.
+// 8. Use search to find the position of the first occurrence of the word because 
+//in the following sentence:'You cannot end a sentence with because because because is a conjunction'.
+
+const paragraph = "You cannot end a sentence with because because because is a conjunction"
+
+const searchTerm = 'because';
+const indexOfFirst = paragraph.indexOf(searchTerm);
+
+console.log(`The index of the first "${searchTerm}" from the beginning is ${indexOfFirst}`);
+
