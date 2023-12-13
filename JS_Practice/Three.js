fa54fcafeb89d20f1ac1f86e1f31574b3cf7dfc1
Akshata <akshatasag@gmail.com>
2023-12-13 10:18:31 +0530

    Updated Three.js

--- JS_Practice/Three.js
+++ JS_Practice/Three.js
@@ -1,1 +1,8 @@
-// 3. Decalre a varibale and assign value to it and change all the string characters to capital letters using toUpperCase() method.
+// 3. Decalre a varibale and 
+//assign value to it and 
+//change all the string characters to capital letters using toUpperCase() method.
+
+let changecase = "akshata";
+let changed = changecase.toUpperCase();
+
+console.log(`${changed}`);
