1bfa3a30b6512f0384433d3a154800042a7918fb
Akshata <akshatasag@gmail.com>
2023-12-13 10:00:29 +0530

    Created One.js

---
+++ JS_Practice/One.js
@@ -0,0 +1,14 @@
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+let str = "Akshata";
+let boo = true;
+let und ;
+let nul = null;
+
+console.log("The value of str :" + str + " " + "and the type  is :" + typeof(str) );
+
+console.log("The value of boo :" + boo + " " + "and the type  is :" + typeof(boo) );
+
+console.log("The value of und :" + und + " " + "and the type  is :" + typeof(und) );
+
+console.log("The value of nul :" + nul + " " + "and the type  is :" + typeof(nul) );
