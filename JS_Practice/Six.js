8b88e81f7d29113ee186f231584765b913bbcd79
Akshata <akshatasag@gmail.com>
2023-12-13 10:43:23 +0530

    Update Six.js

--- JS_Practice/Six.js
+++ JS_Practice/Six.js
@@ -1,1 +1,7 @@
-// 6. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+// 6. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+//split the string at the comma and change it to an array.
+
+const fang = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+const fangArray = fang.split(' ');
+console.log(fangArray);
