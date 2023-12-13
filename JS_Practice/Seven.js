2e0893f78a6f626efb5ec76b4d2137b4493c5b4f
Akshata <akshatasag@gmail.com>
2023-12-13 10:47:01 +0530

    Update Seven.js

--- JS_Practice/Seven.js
+++ JS_Practice/Seven.js
@@ -1,1 +1,7 @@
 // 7. Use lastIndexOf to determine the position of the last occurrence of a script.
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+
+const searchTerm = 'dog';
+
+console.log(`The index of the first "${searchTerm}" from the end is ${paragraph.lastIndexOf(searchTerm)}`);
