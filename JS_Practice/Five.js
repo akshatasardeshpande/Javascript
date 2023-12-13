c1c28ecd4d5a7e90b34212c75b65a71ebfe1e09a
Akshata <akshatasag@gmail.com>
2023-12-13 10:41:33 +0530

    Update Five.js

--- JS_Practice/Five.js
+++ JS_Practice/Five.js
@@ -1,1 +1,9 @@
 // 5. Split the string into an array using split() method
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const word = str.split(' ');
+console.log(word[2]);
+
+const chars = str.split('');
+console.log(chars[2]);
