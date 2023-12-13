03b9ea0ddda35fafa558d31a9a16be3463fc4a42
Akshata <akshatasag@gmail.com>
2023-12-13 10:50:19 +0530

    Update Nine.js

--- JS_Practice/Nine.js
+++ JS_Practice/Nine.js
@@ -1,1 +1,6 @@
 // 9. Use trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+const greeting = '   Hello world!   ';
+
+console.log("Before Trim:",greeting);
+console.log("After Trim: ",greeting.trim());
