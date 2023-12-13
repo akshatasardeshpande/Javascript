a53bb3e29f0352f3f7d7bb90376287ebed6797dc
Akshata <akshatasag@gmail.com>
2023-12-13 10:21:21 +0530

    Update Four.js

--- JS_Practice/Four.js
+++ JS_Practice/Four.js
@@ -1,1 +1,5 @@
 // 4. Check if the string contains a word Script using includes() method.
+
+let chk = "Practising Javascript includes solving questions";
+let chkvar = chk.includes("Script");
+console.log(`${chkvar}`);
