68ed76f160d8626131aa85f94f705894eefe8ad6
Akshata <akshatasag@gmail.com>
2024-06-22 20:36:49 +0530

    solved

--- JS_Practice/11-20/Eleven.js
+++ JS_Practice/11-20/Eleven.js
@@ -1,4 +1,5 @@
-// 11. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+// 11. Figure out the result of the following comparison expression first without using console.log(). 
+// After you decide the result confirm it using console.log()
 //     - 4 > 3
 //     - 4 >= 3
 //     - 4 < 3
