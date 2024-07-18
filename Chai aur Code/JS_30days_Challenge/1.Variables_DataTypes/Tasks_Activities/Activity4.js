4b5682f95c7693d58e3304661c2d0d39ede6c950
Akshata <akshatasag@gmail.com>
2024-07-18 12:44:05 +0530

    Task 5 done

---
+++ Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity4.js
@@ -0,0 +1,8 @@
+//Activity 4: Reassigning variables
+//Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+let reassignTest = 'akshata';
+console.log(`${reassignTest}`);
+
+reassignTest ='hitesh';
+console.log(`${reassignTest}`);
