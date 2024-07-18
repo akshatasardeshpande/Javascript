769df660781e479c71934ba64ce984c2d1c893f9
Akshata <akshatasag@gmail.com>
2024-07-18 12:11:00 +0530

    Added heading

--- Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity1.js
+++ Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity1.js
@@ -1,4 +1,4 @@
-//Activity 1:
+//Activity 1: Variable declaration
 //Task 1: Declare a variable using var, assign it a number, and log the value to the console.
 //Task 2: Declare a variable using let, assign it a string, and log the value to the console.
 
