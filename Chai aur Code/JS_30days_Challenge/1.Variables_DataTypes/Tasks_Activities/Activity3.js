1920788c769259d337cad797897024d10b95846e
Akshata <akshatasag@gmail.com>
2024-07-18 12:38:25 +0530

    Task 4 done

---
+++ Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity3.js
@@ -0,0 +1,11 @@
+// Activity 3: Data Types
+// Task 4: Create variables of different data types(number,string,boolean, object, array) and 
+//log each variable's type using typeof operator
+
+let numTest =2;
+let stringTest = "akshata";
+let boolTest = true;
+let objTest = {0: 'test'};
+let arrTest = [1,2,3];
+
+console.log(`${typeof numTest}` + `\n` + `${typeof stringTest}`+ `\n` +`${typeof boolTest}`+ `\n` + `${typeof objTest}` + `\n` +`${typeof arrTest}`);
