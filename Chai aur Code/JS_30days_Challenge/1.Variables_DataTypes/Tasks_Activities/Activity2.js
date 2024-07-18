d372301e6f7de86610312bea2861b927ec5c7bce
Akshata <akshatasag@gmail.com>
2024-07-18 12:18:31 +0530

    Task 3 done

---
+++ Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity2.js
@@ -0,0 +1,5 @@
+//Activity 2: Constant Declaration
+//Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const constTest = 0;
+console.log(`${constTest}`);
