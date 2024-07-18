7d9b4ba5c6ab85264ade7ee179a2ca892ea696a8
Akshata <akshatasag@gmail.com>
2024-07-18 12:09:58 +0530

    Task 1 & 2 done

---
+++ Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity1.js
@@ -0,0 +1,10 @@
+//Activity 1:
+//Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+//Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+var numTest = 12;
+console.log(`${numTest}`);
+
+let stringTest = 'akshata';
+console.log(`${stringTest}`);
+
