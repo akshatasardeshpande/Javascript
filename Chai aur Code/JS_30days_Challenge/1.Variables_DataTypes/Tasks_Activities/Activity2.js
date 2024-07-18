81f102ac9565cf3297bad28d673ce602f52f909c
Akshata <akshatasag@gmail.com>
2024-07-18 12:39:09 +0530

    corrected bool value

--- Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity2.js
+++ Chai aur Code/JS_30days_Challenge/1.Variables_DataTypes/Tasks_Activities/Activity2.js
@@ -1,5 +1,5 @@
 //Activity 2: Constant Declaration
 //Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
 
-const constTest = 0;
+const constTest = true;
 console.log(`${constTest}`);
