0b252c3a6e956e13f1fbd388495e17f3ea40f326
Akshata <akshatasag@gmail.com>
2024-08-01 23:49:51 +0530

    Hoisting done

---
+++ Namaste JS/Season 1/03Hoisting/index.js
@@ -0,0 +1,48 @@
+// var x =7;
+
+// function getName(){
+//     console.log("Namaste JavaScript");
+// }
+
+// getName();
+// console.log(x);
+// -------------------------------
+// getName();
+// console.log(x);
+
+// var x =7;
+
+// function getName(){
+//     console.log("Namaste JavaScript");
+// }
+
+// -------------------------------
+// console.log(getName);
+
+// var x =7;
+
+// function getName(){
+//     console.log("Namaste JavaScript");
+// }
+// -------------------------------
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+// var x =7;
+
+// var getName = () => {
+//     console.log("Namaste JavaScript");
+// }
+
+// -------------------------------
+getName();
+console.log(x);
+console.log(getName);
+
+var x =7;
+
+var getName = function(){
+    console.log("Namaste JavaScript");
+}
